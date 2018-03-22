f2b3719f21f8631d8c3c7929c14dff0d8c7bdd7f
paosch <paola_schiaffino@yahoo.co.uk>
2018-03-22 15:49:53 +0000

    Thermostat has up, down and power saving mode functions

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,7 +7,49 @@ describe('Thermostat', function(){
     thermostat = new Thermostat();
   });
 
-  it('thermostat starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
-});
+
+  it('increases in temperature with up()', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('does not go below 10 degrees', function(){
+    for (var i = 1; i < 11; i ++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has a power saving mode on by default', function(){
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  it('can switch power saving mode off', function(){
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it('can switch power saving mode back on', function(){
+    thermostat.switchPowerSavingModeOff();
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  describe ('when power saving mode is on', function() {
+    it('has a maximum temperature of 25 degrees', function() {
+      for (var i = 0; i < 5; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
+  });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,43 @@
 'use strict';
 
 function Thermostat(){
+  this.MINIMUM_TEMPERATURE = 10
   this.temperature = 20
+  this.powerSavingMode = true;
 
+}
+
+Thermostat.prototype.getCurrentTemperature = function(){
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function(){
+  this.temperature += 1;
+};
+
+// Thermostat.prototype.down = function(){
+//   this.temperature -= 1;
+// };
+Thermostat.prototype.down = function(){
+  if(this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1;
+}
+
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+}
+
+Thermostat.prototype.isPowerSavingModeOn = function(){
+  return this.powerSavingMode === true;
+}
+
+Thermostat.prototype.switchPowerSavingModeOff = function(){
+  this.powerSavingMode = false;
+}
+
+Thermostat.prototype.switchPowerSavingModeOn = function(){
+  this.powerSavingMode = true;
 }
