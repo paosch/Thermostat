b4b03f0cca0b81bc21a9aa3db2c6499673292dda
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-15 11:19:38 +0100

    Temperature can be increased

--- version2/spec/thermostatSpec.js
+++ version2/spec/thermostatSpec.js
@@ -5,6 +5,9 @@ describe('Thermostat', function(){
     thermostat = new Thermostat();
   });
   it('starts with a temperature of 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+  it('temperature can be increased', function(){
+    expect(thermostat.up()).toEqual(21);
   });
 });
